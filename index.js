2038c87277692bcdc7d46654ec670c0fb2c8c5fb
riyaverma85 <riyaverma2976@gmail.com>
2025-10-06 23:31:48 -0700

    clint_or_ servclineter_connection

--- index.js
+++ index.js
@@ -6,9 +6,10 @@
 // }).listen(8000);
 
 const http = require("http");
+
 http.createServer((reg,res)=>{
-  res.write("<h1>hello we are learning node js</h1>");
-  res.end("end server");
+  res.write("<h1>hello we are learning node js!</h1>");
+  res.end("end server.........");
 }).listen(4000);
 
 
