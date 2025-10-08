4eea70a46d7cde173d2c027ffa3bb67091af08fa
riyaverma85 <riyaverma2976@gmail.com>
2025-10-07 19:32:04 -0700

    module

---
+++ cybrom.js
@@ -0,0 +1,11 @@
+const  mycollege=()=>{
+    return"<h1>cybrom is a college</h1>";
+}
+const myName=()=>{
+    return"<h1>my name is riya </h1>";
+}
+module.exports={
+    mycollege,
+    myName
+};
+
--- index.js
+++ index.js
@@ -1,17 +1,29 @@
-// const http = require("http");
- 
-// http.createServer((reg,res)=>{
-//     res.write("<h1>hello world</h1>");
-//     res.end();
-// }).listen(8000);
-
 const http = require("http");
-
+ const fs=require("fs");
 http.createServer((reg,res)=>{
-  res.write("<h1>hello we are learning node js!</h1>");
-  res.end("end server.........");
-}).listen(4000);
+    res.write("<h1>hello world</h1>");
+    fs.writeFile("riya.txt",(err,data)=>{
+      if (err)throw err;
+      res.write(data);
+      res.end();
+    })
 
+}).listen(8000,()=>{
+  console.log("server is run on port 8000");
+});
+
+// const http = require("http");
+// const myclg= require("./cybrom")
+// const myName= require("./cybrom")
+// http.createServer((req,res)=>{
+
+//   res.write("<h1>hello we are learning node js!</h1>");
+//   res.write(myclg.mycollege());
+//   res.write(myName.myName());
+//   res.end();
+// }).listen(4000, () =>{
+//   console.log("server is running on port 4000");
+// });
 
 
 
