8ce94f7466c36174728be2bfbefebd1307ebdcd0
riyaverma85 <riyaverma2976@gmail.com>
2025-10-08 18:48:54 -0700

    create_file

--- index.js
+++ index.js
@@ -1,22 +1,7 @@
-const http = require("http");
- const fs=require("fs");
-http.createServer((reg,res)=>{
-    res.write("<h1>hello world</h1>");
-    fs.readFile("riya.txt",(err,data)=>{
-      if(err) throw err;
-      res.write(data);
-      res.end();
-    })
-
-}).listen(8000,()=>{
-  console.log("server is run on port 8000");
-});
-
 // const http = require("http");
 // const myclg= require("./cybrom")
 // const myName= require("./cybrom")
 // http.createServer((req,res)=>{
-
 //   res.write("<h1>hello we are learning node js!</h1>");
 //   res.write(myclg.mycollege());
 //   res.write(myName.myName());
@@ -27,11 +12,40 @@ http.createServer((reg,res)=>{
 
 
 
+// const http = require("http"); 
+//  const fs=require("fs");
+// http.createServer((req,res)=>{
+//     res.write("<h1>hello world</h1>");
+//     fs.readFile("riya.txt",(err,data)=>{
+//       if(err) throw err;
+//       res.write(data);
+//       res.end();
+//     })
+
+// }).listen(8000,()=>{
+//   console.log("server is run on port 8000");
+// });
 
 
 
+// const fs =require("fs");
+// fs.appendFile("riya.txt","welcome to",(err)=>{
+// if (err)throw err;
+// console.log("file created")
+// })
 
 
+// const fs =require("fs");
+// fs.open("rani.pdf","w",(error,file)=>{
+//   if(err)throw err;
+//   console.log("file created");
+// })
+
+const fs =require("fs");
+fs.writeFile("ranu.pdf","welcome",(err)=>{
+  if (err)throw err;
+  console.log("file created");
+})
 
 
 
